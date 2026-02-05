07824a6158cf4e5c98e2dc0ab17d3179574d1483
Annika Feist <annika.feist@hfg-gmuend.de>
2026-02-05 14:35:14 +0100

    nochmal ncohmal

--- src/components/DrawingCanvas/DrawingCanvas.jsx
+++ src/components/DrawingCanvas/DrawingCanvas.jsx
@@ -23,7 +23,7 @@ const BRUSHES = {
         pressureSensitivity: 0.8,
     },
     pinsel: {
-        name: 'Maoobi',
+        name: 'Maobi',
         baseSize: 5,
         minSize: 2,
         maxSize: 5,
